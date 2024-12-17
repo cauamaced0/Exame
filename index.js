4670cb39d29172d3b44276c47edfeab138e1d53b
cauamaced0 <cauassmacedo01@gmail..com>
2024-12-17 17:25:44 -0300

    criadoo o aplicativo

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-TESTEANDO
+import express from "express";
+
+const host= "0.0.0.0"; // Funcionara em todas as redes do computador
+const porta = 3000;
+const app= express();
+
+app.listen(porta,host,() =>{
+    console.log("Servidor em execução http://" + host + ":" + porta);
+})
